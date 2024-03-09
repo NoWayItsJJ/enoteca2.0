d92b9aae872ff0ba9d40761a89dc42d8d370e98f
NoWayItsJJ <fabio.pauletta@gmail.com>
2024-03-09 12:40:15 +0100

    began articles operations

---
+++ backend/elimina_articolo.php
@@ -0,0 +1,11 @@
+<?php
+    //implementare un trigger che copi i dati dell'articolo eliminato in un'altra tabella prima di eliminarlo
+    //in modo tale che per vedere lo storico dei prestiti si possa fare l'unione delle due tabelle e joinare con la tabella prestiti
+
+    //require_once '../db.php';
+    //$id = $_GET['id'];
+    //$deleteSql = "DELETE FROM articoli WHERE id_articolo = $id";
+    //$conn->query($deleteSql);
+    //header('Location: visualizzaArticoli.php');
+    //exit;
+?>
